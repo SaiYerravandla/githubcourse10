e50ff3228349f4355a82a30ed392b759e4a57a19
SaiYerravandla <saiyerravandla@gmail.com>
2023-06-15 19:23:52 +0530

    added,modified,deleted

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -5,5 +5,10 @@ public class LoginPage {
 	public static void main(String[] args) {
 		
 		System.out.println("hi im guru sai");
+		
+		String un="hi sai";
+		String known="guru sai";
+		
+		System.out.println(un + known);
 	}
 }
---
+++ src/pages/SearchPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+public class SearchPage {
+	
+	
+	public void search() {
+		System.out.println("Search engine");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
